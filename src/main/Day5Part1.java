45ddd325065d8628c5602664bcc5cb98bf074aa5
Ganensh Gore <ganeshgore555@gmail.com>
2022-12-05 13:53:07 +0530

    Day5

--- src/main/Day5Part1.java
+++ src/main/Day5Part1.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Stack;
@@ -27,8 +29,9 @@ import java.util.Stack;
 public class Day5Part1 {
 	public static void main(String[] args) {
 		try {
+			String line = null;			
 			BufferedReader br = new BufferedReader(new FileReader(new File(".//resources//Day5Input.txt")));
-			String line = null;
+
 			HashMap<Integer,Stack> map = new HashMap<Integer,Stack>();
 			Stack<String> stack1 = new Stack<String>();
 			stack1.push("W");
